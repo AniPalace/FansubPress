d8a583ebed1a2a0b74388292f9e313d2fa22d2ee
NewPlayer <karasztamas@gmail.com>
2015-10-14 11:52:17 +0200

    kezdőlapon jelenjen meg a download post type elemei is

--- wp-content/plugins/fansubpress/class/FansubPressPlugin.php
+++ wp-content/plugins/fansubpress/class/FansubPressPlugin.php
@@ -9,5 +9,23 @@ class FansubPressPlugin extends FansubPressPluginBase{
    * @since 2015-10-13
    */
   function init() {
+    // Extra típusok megjelenítése a kezdőlapon
+    add_filter( 'pre_get_posts', array($this,'extraTypesInHomePage') );
+  }
+
+  /**
+   * A kezdőlapon megjelenítendő extra típusok
+   *
+   * @author NewPlayer
+   * @since 2015-10-14
+   *
+   * @param  [type] $query [description]
+   * @return [type]        [description]
+   */
+  function extraTypesInHomePage( $query ) {
+  	if ( ( is_home() && $query->is_main_query() ) || is_feed() ) {
+  		$query->set( 'post_type', array( 'post', 'download' ) );
+  	   return $query;
+    }
   }
 }
