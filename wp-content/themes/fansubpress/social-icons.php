93af7debb18b4cc6a0b2b05cee3c1b880a5f48a5
NewPlayer <karasztamas@gmail.com>
2016-02-01 22:37:19 +0100

    fejlécben megjelenő iconok

---
+++ wp-content/themes/fansubpress/social-icons.php
@@ -0,0 +1,7 @@
+	<?php if (get_theme_mod('fansubpress_indavideo')) { ?>
+		<a target="_blank" class="fa fa-youtube-play" href="<?php echo esc_url(get_theme_mod('fansubpress_indavideo')); ?>" title="IndaVideo" ></a>
+	<?php } ?>
+		
+	<?php if (get_theme_mod('fansubpress_facebook')) { ?>
+		<a target="_blank" class="fa fa-facebook-square" href="<?php echo esc_url(get_theme_mod('fansubpress_facebook')); ?>" title="Facebook" ></a>
+	<?php } ?>
